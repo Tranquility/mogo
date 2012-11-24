ac39daf1a281f8f03235ed5178ed6c535a96481b
0schleew <0schleew@rzmac15.informatik.uni-hamburg.de>
2012-11-24 12:23:11 +0100

    Added view controller for first two major views

---
+++ MoGO/AppointmentViewController.h
@@ -0,0 +1,13 @@
+//
+//  AppointmentViewController.h
+//  MoGO
+//
+//  Created by 0schleew on 24.11.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppointmentViewController : UIViewController
+
+@end
