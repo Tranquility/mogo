f7bae5f8d5cf787dab946a6b0dab4269deddb7bb
0schleew <0schleew@rzmac15.informatik.uni-hamburg.de>
2012-11-24 11:59:09 +0100

    Add storyboard

---
+++ MoGO/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  MoGO
+//
+//  Created by 0schleew on 24.11.12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
