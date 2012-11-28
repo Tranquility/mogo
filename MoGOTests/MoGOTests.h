1424dcbbd02c72282f90f4a33044cdd83ecda1e8
0leschen <0leschen@rzmac18.informatik.uni-hamburg.de>
2012-11-28 11:46:05 +0100

    Unit Test hinzugefügt.

---
+++ MoGOTests/MoGOTests.h
@@ -0,0 +1,16 @@
+//
+//  MoGOTests.h
+//  MoGOTests
+//
+//  Created by 0leschen on 28.11.12.
+//
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+#import "DoctorModel.h"
+
+@interface MoGOTests : SenTestCase
+
+@property (nonatomic, strong) DoctorModel *doctorModel;
+
+@end
