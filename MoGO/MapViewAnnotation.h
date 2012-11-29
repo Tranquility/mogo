aa6ca550e681bc93f6260ac3c634636b7b2e4afb
0schleew <0schleew@rzmac29.informatik.uni-hamburg.de>
2012-11-29 16:23:37 +0100

    Improves the source code Fixes bugs that will occur eventually since some forgot to declare the constructor in the public interface -.-

--- MoGO/MapViewAnnotation.h
+++ MoGO/MapViewAnnotation.h
@@ -3,17 +3,11 @@
 #import <Foundation/Foundation.h>
 #import <MapKit/MapKit.h>
 
-@interface MapViewAnnotation : NSObject <MKAnnotation> {
-    
-	NSString *title;
-	CLLocationCoordinate2D coordinate;
-    
-}
+@interface MapViewAnnotation : NSObject <MKAnnotation>
 
-@property (nonatomic, copy) NSString *title;
+@property (nonatomic) NSString *title;
 @property (nonatomic, readonly) CLLocationCoordinate2D coordinate;
 
--(void)drawMap:(double)withLatitude andLongitute:(double)longitude andString:(NSString*)arztname;
-- (id)initWithTitle:(NSString *)ttl andCoordinate:(CLLocationCoordinate2D)c2d;
+- (MapViewAnnotation*)initWithTitle:(NSString*)title andCoordinate:(CLLocationCoordinate2D)coordinate;
 
 @end
--- MoGO/MapViewAnnotation.m
+++ MoGO/MapViewAnnotation.m
@@ -4,18 +4,15 @@
 
 @implementation MapViewAnnotation
 
-@synthesize title, coordinate;
 
-- (id)initWithTitle:(NSString *)ttl andCoordinate:(CLLocationCoordinate2D)c2d {
-//	[super init];
-	title = ttl;
-	coordinate = c2d;
-	return self;
+- (MapViewAnnotation*)initWithTitle:(NSString*)title andCoordinate:(CLLocationCoordinate2D)coordinate {
+    self = [super init];
+    if (self) {
+        self.title = title;
+        self.coordinate = coordinate;
+    }
+    
+    return self;
 }
-
-//- (void)dealloc {
-//	[title release];
-//	[super dealloc];
-//}
 
 @end
