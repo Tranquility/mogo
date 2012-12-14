adf4f01d0d295746873ef9aeafef94c8b7505b3f
Ole Reifschneider <mail@ole-reifschneider.de>
2012-12-14 23:30:39 +0100

    Make addresscoordinates to nsnumber fields

--- MedicDetailViewController.m
+++ MedicDetailViewController.m
@@ -76,7 +76,10 @@
     self.addressField.text = address;
     self.phoneField.text = self.doctor.telephone;
     
-    [self drawMapWithCoordinate:*doctorAddress.coordinate andString:name];
+    CLLocationCoordinate2D location;
+    location.latitude = [doctorAddress.latitude floatValue];
+    location.longitude = [doctorAddress.longitude floatValue];
+    [self drawMapWithCoordinate:location andString:name];
     [self.mapOutlet setMapType:MKMapTypeStandard];
 }
 
@@ -96,25 +99,9 @@
 
 -(void)drawMapWithCoordinate:(CLLocationCoordinate2D)location andString:(NSString*)doctorName
 {
-    //double miles = 5.0;
-    //double scalingFactor = ABS( (cos(2 * M_PI * location.latitude / 360.0) ));
-    
-    //MKCoordinateSpan span;
-    
-    //span.latitudeDelta = miles/69.0;
-    //span.longitudeDelta = miles/(scalingFactor * 69.0);
-    
-    //MKCoordinateRegion region;
-    //region.span = span;
-    //region.center = location;
-    
-    //This can be done easier:
-    MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance (location, 50, 50);
-    
-
+    MKCoordinateRegion region = MKCoordinateRegionMakeWithDistance (location, 500, 500);
     MapViewAnnotation *newAnnotation = [[MapViewAnnotation alloc] initWithTitle:doctorName andCoordinate:location];
 	[self.mapOutlet addAnnotation:newAnnotation];
-    
     [self.mapOutlet setRegion:region animated:YES];
     
 }
