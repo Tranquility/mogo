d279469d2b420b841243601580d2f68cf39a73da
Ole Reifschneider <mail@ole-reifschneider.de>
2012-12-15 22:34:24 +0100

    Basic login implementation

--- MoGO/LoginViewController.m
+++ MoGO/LoginViewController.m
@@ -8,9 +8,11 @@
 
 #import "LoginViewController.h"
 #import "ViewController.h"
+#import "ApiClient.h"
+#import "CredentialStore.h"
 
 @interface LoginViewController ()
-
+    @property (nonatomic) CredentialStore *credentialStore;
 @end
 
 @implementation LoginViewController
@@ -27,6 +29,14 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    self.credentialStore = [[CredentialStore alloc] init];
+    
+    if ([self.credentialStore isLoggedIn]) {
+        NSLog(@"User is already Logged in");
+    } else {
+        NSLog(@"User is not Logged in");
+    }
     
     [self setTitle:@"LogIn"];
 //    Later: Check if already logged in
@@ -51,43 +61,64 @@
 }
 
 - (void)viewDidUnload {
-    [self setMailAdressField:nil];
-    [self setPasswordField:nil];
     [super viewDidUnload];
 }
 
 - (IBAction)loginButtonPressed:(id)sender {
     //generate querry to server
-    NSString *userMail, *userPassword;
-    userMail = self.mailAdressField.text;
-    userPassword = self.passwordField.text;
-    //send querry, wait for response, then do
-    if (YES) //change with server confirms account
-    {
-        UIAlertView *message = [[UIAlertView alloc] initWithTitle:NSLocalizedString(@"Login erfolgreich", @"LOGIN_SUCCESSFULL")
-                                                          message:NSLocalizedString(@"Viel Spaß bei der Nutzung von MoGO", @"ENJOY_MOGO")
-                                                         delegate:nil
-                                                cancelButtonTitle:@"OK"
-                                                otherButtonTitles:nil];
-        
-        NSUserDefaults *saveLogin = [NSUserDefaults standardUserDefaults];
-        [saveLogin setObject:userMail forKey:@"currentUserMail"];
-        [message show];
-        //TODO: Push to right controller if login worked
-        
-    }
-    else
-    {
-        //ask server for fail reason (mail unknown? password wrong? give more detailed feedback)
-        UIAlertView *message = [[UIAlertView alloc] initWithTitle:NSLocalizedString(@"Login gescheitert", @"LOGIN_FAILED")
-                                                          message:NSLocalizedString(@"Dies kann verschiedene Gründe haben", @"FAIL_REASONS")
-                                                         delegate:nil
-                                                cancelButtonTitle:@"OK"
-                                                otherButtonTitles:nil];
-        
-        [message show];
-        
-    }
+    id params = @{
+    @"email": self.mailAdressField.text,
+    @"password": self.passwordField.text
+    };
+    
+    
+    NSLog(@"test");
+    [[ApiClient sharedInstance] postPath:@"/tokens.json"
+                                parameters:params
+                                   success:^(AFHTTPRequestOperation *operation, id responseObject) {
+                                       NSString *authToken = [responseObject objectForKey:@"token"];
+                                       [self.credentialStore setAuthToken:authToken];
+                                       [self dismissViewControllerAnimated:YES completion:nil];
+                                       
+                                   } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+                                       if (operation.response.statusCode == 500) {
+                                           NSLog(@"Unknown Error");
+                                       } else {
+                                           NSData *jsonData = [operation.responseString dataUsingEncoding:NSUTF8StringEncoding];
+                                           NSDictionary *json = [NSJSONSerialization JSONObjectWithData:jsonData
+                                                                                                options:0
+                                                                                                  error:nil];
+                                           NSString *errorMessage = [json objectForKey:@"error"];
+                                           NSLog(@"%@",errorMessage);
+                                       }
+                                   }];
+    
+//    if (YES) //change with server confirms account
+//    {
+//        UIAlertView *message = [[UIAlertView alloc] initWithTitle:NSLocalizedString(@"Login erfolgreich", @"LOGIN_SUCCESSFULL")
+//                                                          message:NSLocalizedString(@"Viel Spaß bei der Nutzung von MoGO", @"ENJOY_MOGO")
+//                                                         delegate:nil
+//                                                cancelButtonTitle:@"OK"
+//                                                otherButtonTitles:nil];
+//        
+//        NSUserDefaults *saveLogin = [NSUserDefaults standardUserDefaults];
+//        [saveLogin setObject:userMail forKey:@"currentUserMail"];
+//        [message show];
+//        //TODO: Push to right controller if login worked
+//        
+//    }
+//    else
+//    {
+//        //ask server for fail reason (mail unknown? password wrong? give more detailed feedback)
+//        UIAlertView *message = [[UIAlertView alloc] initWithTitle:NSLocalizedString(@"Login gescheitert", @"LOGIN_FAILED")
+//                                                          message:NSLocalizedString(@"Dies kann verschiedene Gründe haben", @"FAIL_REASONS")
+//                                                         delegate:nil
+//                                                cancelButtonTitle:@"OK"
+//                                                otherButtonTitles:nil];
+//        
+//        [message show];
+//        
+//    }
 }
 
 
