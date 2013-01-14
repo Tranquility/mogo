5a782c659866b6b16a5c6162b4b67b145be95f70
0leschen <0leschen@rzmac18.informatik.uni-hamburg.de>
2013-01-14 15:57:29 +0100

    Settings hinzugefügt.

---
+++ MoGO/SettingsViewController.h
@@ -0,0 +1,13 @@
+//
+//  SettingsViewController.h
+//  MoGO
+//
+//  Created by 0leschen on 14.01.13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SettingsViewController : UIViewController
+
+@end
